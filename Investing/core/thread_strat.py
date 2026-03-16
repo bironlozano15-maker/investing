7c2c6dc301871ba0b91cb1f52b96f030b949087f
Daniel Bruno <danielbruno9346@gmail.com>
2026-03-16 21:18:35 +0900

    print submission of strategy history in live

--- Investing/core/thread_strat.py
+++ Investing/core/thread_strat.py
@@ -4,12 +4,13 @@ from datetime import datetime, timezone
 
 if __name__ == "__main__":
     while True:
-        generate_strat(datetime.utcnow())
-        print("Generated new strategy at", datetime.now(timezone.utc).replace(microsecond=0))
-        time.sleep(30)
+        # generate_strat(datetime.utcnow())
+        # print("Generated new strategy at", datetime.now(timezone.utc).replace(microsecond=0))
+        # time.sleep(30)
 
-        # if datetime.utcnow().hour == 13 and datetime.utcnow().minute == 5:
-        #     generate_strat(datetime.utcnow())
-        #     time.sleep(300)
-        # else:
-        #     time.sleep(30)
+        if datetime.utcnow().hour == 13 and datetime.utcnow().minute == 5:
+            generate_strat(datetime.utcnow())
+            print("Generated new strategy at", datetime.now(timezone.utc).replace(microsecond=0))
+            time.sleep(300)
+        else:
+            time.sleep(30)
